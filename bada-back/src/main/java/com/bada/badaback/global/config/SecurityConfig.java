0ed9aa73af8b8cb6b17d54a39ddf806a404371f8
sunkyoung <yskkjdh@gmail.com>
2024-03-12 02:25:03 +0900

    setting : 전역예외처리 및 application.yml 생성
    Change-Id: I06241e6d1799c7b903269b04513ced52fb7fe894

---
+++ bada-back/src/main/java/com/bada/badaback/global/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.bada.badaback.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/h2-console/**", "/error");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement((sessionManagement) ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequest ->
+                        authorizeRequest
+                                .requestMatchers(
+                                        AntPathRequestMatcher.antMatcher("/login/**")
+                                ).permitAll()
+                                .requestMatchers(
+                                        AntPathRequestMatcher.antMatcher("/h2-console/**")
+                                ).permitAll()
+                );
+
+        return http.build();
+    }
+
+}
