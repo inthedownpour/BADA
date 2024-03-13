eedebd27b754e409c89fbbd8e06a2f8cf2d17e72
sunkyoung <yskkjdh@gmail.com>
2024-03-13 09:18:35 +0900

    test : 회원가입 API 관련 테스트 코드 작성
    Change-Id: I31b0a3cbf374fab1cd580150489bb7a965a76bc5

--- bada-back/src/test/java/com/bada/badaback/common/ControllerTest.java
+++ bada-back/src/test/java/com/bada/badaback/common/ControllerTest.java
@@ -1,24 +1,30 @@
 package com.bada.badaback.common;
 
+import com.bada.badaback.auth.controller.AuthApiController;
+import com.bada.badaback.auth.service.AuthService;
 import com.bada.badaback.global.config.SecurityConfig;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 @ImportAutoConfiguration(SecurityConfig.class)
 @WebMvcTest({
+        AuthApiController.class
 })
-@AutoConfigureRestDocs
 public abstract class ControllerTest {
     @Autowired
     protected MockMvc mockMvc;
 
     @Autowired
     protected ObjectMapper objectMapper;
+
+    @MockBean
+    protected AuthService authService;
 
     protected String convertObjectToJson(Object data) throws JsonProcessingException {
         return objectMapper.writeValueAsString(data);
