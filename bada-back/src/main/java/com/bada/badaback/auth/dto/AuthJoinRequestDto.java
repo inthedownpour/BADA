76b2bcf615deba3f0e6e88319e6d7fd571504151
sunkyoung <yskkjdh@gmail.com>
2024-03-14 12:31:15 +0900

    refactor : vaildation 어노테이션 추가
    Change-Id: I9cb9e004fe575b244b008bf6ffb2609c966adf25

--- bada-back/src/main/java/com/bada/badaback/auth/dto/AuthJoinRequestDto.java
+++ bada-back/src/main/java/com/bada/badaback/auth/dto/AuthJoinRequestDto.java
@@ -1,12 +1,21 @@
 package com.bada.badaback.auth.dto;
 
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
 public record AuthJoinRequestDto(
+        @NotBlank(message = "이름은 필수입니다.")
         String name,
         String phone,
         String email,
         String social,
+        @Min(value = 0, message = "부모 여부는 0또는 1만 가능합니다.")
+        @Max(value = 1, message = "부모 여부는 0또는 1만 가능합니다.")
         int isParent,
         String profileUrl,
-        String authCode
+        @NotBlank(message = "인증코드는 필수입니다.")
+        String code
 ) {
 }
