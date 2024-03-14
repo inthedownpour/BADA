3d053370d4543733705139a1bfbc079ae88db940
이용준 <sark2@naver.com>
2024-03-14 14:56:27 +0900

    feat: Alarm kafka 적용

---
+++ bada-back/src/main/java/com/bada/badaback/kafka/controller/KafkaController.java
@@ -0,0 +1,30 @@
+package com.ssafy.bada.badaback.kafka.controller;
+
+import com.ssafy.bada.badaback.kafka.service.KafkaProducer;
+import com.ssafy.bada.badaback.kafka.dto.AlarmDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/kafka")
+@Slf4j
+@RequiredArgsConstructor
+public class KafkaController {
+
+  private final KafkaProducer producer;
+
+  @PostMapping("/alarm")  // alarm이용
+  @ResponseBody
+  public String sendAlarm(@RequestBody AlarmDto alarmDto) {
+    log.info("################## AlarmDto : {}", alarmDto.toString());
+    log.info("################## send alarm : {}, {}, {}", alarmDto.getType(), alarmDto.getUserId(), alarmDto.getContent());
+    producer.sendAlarm(alarmDto.getType(), alarmDto.getUserId(), alarmDto.getContent());
+
+    return "success";
+  }
+}
---
+++ bada-back/src/main/java/com/bada/badaback/kafka/service/KafkaProducer.java
@@ -0,0 +1,36 @@
+package com.ssafy.bada.badaback.kafka.service;
+
+import com.ssafy.bada.badaback.kafka.dto.AlarmDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class KafkaProducer {
+  private static final String TOPIC = "alarm-topic";   // 전달되는 토픽의 이름
+
+  @Autowired
+  private final KafkaTemplate<String, AlarmDto> kafkaTemplate;
+//  private final KafkaTemplate<String, String> kafkaTemplate;
+
+//  public void sendMessage(String message) {
+//    log.info("Produce message : {}", message);
+//    this.kafkaTemplate.send(TOPIC, message);
+//  }
+
+  public void sendAlarm(String type, String userId, String content) {
+    AlarmDto alarmDto = AlarmDto.builder()
+        .type(type)
+        .userId(userId)
+        .content(content)
+        .build();
+
+    log.info("################## 리뷰 답글 알림 전송. type : {}, userId : {}, message : {}", type, userId, content);
+    kafkaTemplate.send("alarm-topic", alarmDto);
+  }
+
+}
