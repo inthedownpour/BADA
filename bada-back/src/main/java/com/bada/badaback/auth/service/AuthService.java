94303dca6606383178339739edb19b6a30ea1027
sunkyoung <yskkjdh@gmail.com>
2024-03-16 11:55:02 +0900

    feat : 아이 회원가입 API 구현 및 기존 부모 회원가입 API 수정
    Change-Id: Ie235ddec13403bd24c55d03a28fc9e6bc9dbed72

--- bada-back/src/main/java/com/bada/badaback/auth/service/AuthService.java
+++ bada-back/src/main/java/com/bada/badaback/auth/service/AuthService.java
@@ -21,14 +21,14 @@ public class AuthService {
     private final TokenService tokenService;
 
     @Transactional
-    public Long signup(String name, String phone, String email, String social, int isParent, String profileUrl,
+    public Long signup(String name, String phone, String email, String social, String profileUrl,
                        String familyName){
         Long memberId = AlreadyMember(email, social);
 
         if(memberId == null) {
             // 패밀리 생성 메소드 구현 이후 패밀리코드를 넣도록 수정 예정
             // 임시로 familyName을 familyCode 자리에 넣었음
-            Member member = Member.createMember(name, phone, email, SocialType.valueOf(social), isParent, profileUrl, familyName);
+            Member member = Member.createMember(name, phone, email, SocialType.valueOf(social), 1, profileUrl, familyName);
             memberId = memberRepository.save(member).getId();
 
             String accessToken = jwtProvider.createAccessToken(memberId, member.getRole());
@@ -40,18 +40,26 @@ public class AuthService {
     }
 
     @Transactional
-    public Long join(String name, String phone, String email, String social, int isParent, String profileUrl,
+    public Long join(String name, String phone, String email, String social, String profileUrl,
                        String code){
         Long memberId = AlreadyMember(email, social);
         if(memberId == null) {
 
             // 인증번호 유효성 검사 API 구현 후 authCode 기반으로 찾은 familyCode를 저장하도록 수정할 예정
-            Member member = Member.createMember(name, phone, email, SocialType.valueOf(social), isParent, profileUrl, code);
+            Member member = Member.createMember(name, phone, email, SocialType.valueOf(social), 1, profileUrl, code);
             memberId = memberRepository.save(member).getId();
         }
 
         return memberId;
     }
+
+    @Transactional
+    public Long joinChild(String name, String phone, String profileUrl, String code){
+        Member member = Member.createMember(name, phone, "", SocialType.valueOf("CHILD"), 0, profileUrl, code);
+
+        return memberRepository.save(member).getId();
+    }
+
 
     @Transactional
     public LoginResponseDto login(Long memberId) {
