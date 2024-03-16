697859bdbc78e1e9ab4a90b994bb9f043debfc10
sunkyoung <yskkjdh@gmail.com>
2024-03-17 05:10:21 +0900

    test : 가족 생성 메서드 테스트 코드 작성 및 기존 회원가입 테스트 코드 수정
    Change-Id: I6291dfd050b19e9434544f4ac27b5e6449527593

--- bada-back/src/test/java/com/bada/badaback/auth/service/AuthServiceTest.java
+++ bada-back/src/test/java/com/bada/badaback/auth/service/AuthServiceTest.java
@@ -4,8 +4,10 @@ import com.bada.badaback.auth.domain.AuthCode;
 import com.bada.badaback.auth.domain.Token;
 import com.bada.badaback.auth.dto.LoginResponseDto;
 import com.bada.badaback.common.ServiceTest;
+import com.bada.badaback.global.security.JwtProvider;
 import com.bada.badaback.member.domain.Member;
 import com.bada.badaback.member.domain.SocialType;
+import com.bada.badaback.member.service.MemberFindService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,6 +23,15 @@ public class AuthServiceTest extends ServiceTest {
     @Autowired
     private AuthService authService;
 
+    @Autowired
+    private AuthCodeFindService authCodeFindService;
+
+    @Autowired
+    private JwtProvider jwtProvider;
+
+    @Autowired
+    private MemberFindService memberFindService;
+
     private Member member;
     private AuthCode authCode;
 
@@ -28,16 +39,17 @@ public class AuthServiceTest extends ServiceTest {
     void setup() {
         member = memberRepository.save(SUNKYOUNG.toMember());
         authCode = authCodeRepository.save(AUTHCODE_0.toAuthCode(member));
+
     }
 
     @Test
     @DisplayName("회원가입에 성공한다(새로운 가족 그룹 생성)")
     void signup() {
         // when
-        Long saveMemberId = authService.signup("윤선경", "010-1111-1111","abc@naver.com", "NAVER", null, "AB1111");
+        Long saveMemberId = authService.signup("윤선경", "010-1111-1111","abc@naver.com", "NAVER", null, "우리 가족");
 
         // then
-        Member findMember = memberRepository.findById(saveMemberId).orElseThrow();
+        Member findMember = memberFindService.findById(saveMemberId);
         assertAll(
                 () -> assertThat(findMember.getName()).isEqualTo("윤선경"),
                 () -> assertThat(findMember.getPhone()).isEqualTo("010-1111-1111"),
@@ -45,7 +57,7 @@ public class AuthServiceTest extends ServiceTest {
                 () -> assertThat(findMember.getSocial()).isEqualTo(SocialType.NAVER),
                 () -> assertThat(findMember.getIsParent()).isEqualTo(1),
                 () -> assertThat(findMember.getProfileUrl()).isEqualTo(null),
-                () -> assertThat(findMember.getFamilyCode()).isEqualTo("AB1111") // 가족코드 생성구현 이후 변경 예정
+                () -> assertThat(findMember.getFamilyCode()).isNotNull()
         );
     }
 
@@ -56,7 +68,8 @@ public class AuthServiceTest extends ServiceTest {
         Long saveMemberId = authService.join("심지연", "010-2222-2222","def@naver.com", "KAKAO", null, authCode.getCode());
 
         // then
-        Member findMember = memberRepository.findById(saveMemberId).orElseThrow();
+        Member findMember = memberFindService.findById(saveMemberId);
+        String findFamilyCode = authCodeFindService.findMemberByCode(authCode.getCode()).getFamilyCode();
         assertAll(
                 () -> assertThat(findMember.getName()).isEqualTo("심지연"),
                 () -> assertThat(findMember.getPhone()).isEqualTo("010-2222-2222"),
@@ -64,7 +77,7 @@ public class AuthServiceTest extends ServiceTest {
                 () -> assertThat(findMember.getSocial()).isEqualTo(SocialType.KAKAO),
                 () -> assertThat(findMember.getIsParent()).isEqualTo(1),
                 () -> assertThat(findMember.getProfileUrl()).isEqualTo(null),
-                () -> assertThat(findMember.getFamilyCode()).isEqualTo(authCode.getCode()) // 인증코드 생성구현 이후 변경예정
+                () -> assertThat(findMember.getFamilyCode()).isEqualTo(findFamilyCode)
         );
     }
 
@@ -75,7 +88,8 @@ public class AuthServiceTest extends ServiceTest {
         Long saveMemberId = authService.joinChild("심지연", "010-2222-2222", null, authCode.getCode());
 
         // then
-        Member findMember = memberRepository.findById(saveMemberId).orElseThrow();
+        Member findMember = memberFindService.findById(saveMemberId);
+        String findFamilyCode = authCodeFindService.findMemberByCode(authCode.getCode()).getFamilyCode();
         assertAll(
                 () -> assertThat(findMember.getName()).isEqualTo("심지연"),
                 () -> assertThat(findMember.getPhone()).isEqualTo("010-2222-2222"),
@@ -83,7 +97,7 @@ public class AuthServiceTest extends ServiceTest {
                 () -> assertThat(findMember.getSocial()).isEqualTo(SocialType.CHILD),
                 () -> assertThat(findMember.getIsParent()).isEqualTo(0),
                 () -> assertThat(findMember.getProfileUrl()).isEqualTo(null),
-                () -> assertThat(findMember.getFamilyCode()).isEqualTo(authCode.getCode())
+                () -> assertThat(findMember.getFamilyCode()).isEqualTo(findFamilyCode)
         );
     }
 
@@ -91,14 +105,17 @@ public class AuthServiceTest extends ServiceTest {
     @DisplayName("로그인에 성공한다")
     void login() {
         // when
-        Long saveMemberId = authService.signup("윤선경", "010-1111-1111","abc@naver.com", "NAVER", null, "AB1111");
+        Long saveMemberId = authService.signup("윤선경", "010-1111-1111","abc@naver.com", "NAVER", null, "우리 가족");
         LoginResponseDto loginResponseDto = authService.login(saveMemberId);
 
         // then
+        Member findMember = memberFindService.findById(saveMemberId);
         assertAll(
+                () -> assertThat(jwtProvider.getId(loginResponseDto.accessToken())).isEqualTo(saveMemberId),
+                () -> assertThat(jwtProvider.getId(loginResponseDto.refreshToken())).isEqualTo(saveMemberId),
                 () -> assertThat(loginResponseDto.memberId()).isEqualTo(saveMemberId),
                 () -> assertThat(loginResponseDto.name()).isEqualTo("윤선경"),
-                () -> assertThat(loginResponseDto.familyCode()).isEqualTo("AB1111"),
+                () -> assertThat(loginResponseDto.familyCode()).isEqualTo(findMember.getFamilyCode()),
                 () -> {
                     Token findToken = tokenRepository.findByMemberId(saveMemberId).orElseThrow();
                     assertThat(loginResponseDto.refreshToken()).isEqualTo(findToken.getRefreshToken());
