d4326c1650e537a7defd3918efe1af2dc20df176
sunkyoung <yskkjdh@gmail.com>
2024-03-18 14:38:45 +0900

    feat : 가족 리스트 조회 API 구현
    Change-Id: I2efa5046a2eef89eb2a80584718e18b4a561ba61

---
+++ bada-back/src/main/java/com/bada/badaback/member/controller/MemberListApiController.java
@@ -0,0 +1,26 @@
+package com.bada.badaback.member.controller;
+
+import com.bada.badaback.global.annotation.ExtractPayload;
+import com.bada.badaback.member.dto.MemberResponseDto;
+import com.bada.badaback.member.service.MemberListService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Tag(name = "Member", description = "MemberListApiController")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/members")
+public class MemberListApiController {
+    private final MemberListService memberListService;
+
+    @GetMapping
+    public ResponseEntity<List<MemberResponseDto>> familyList(@ExtractPayload Long memberId) {
+        return ResponseEntity.ok(memberListService.familyList(memberId));
+    }
+}
