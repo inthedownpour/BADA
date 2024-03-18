31773dca748b71f691ac6e3ca2aa04816a07c3be
sunkyoung <yskkjdh@gmail.com>
2024-03-18 14:39:04 +0900

    edit : Tag 수정
    Change-Id: If2396534e79a811277134d3557441364a2c842c8

--- bada-back/src/main/java/com/bada/badaback/auth/controller/TokenReissueApiController.java
+++ bada-back/src/main/java/com/bada/badaback/auth/controller/TokenReissueApiController.java
@@ -4,12 +4,14 @@ import com.bada.badaback.auth.dto.TokenResponseDto;
 import com.bada.badaback.auth.service.TokenReissueService;
 import com.bada.badaback.global.annotation.ExtractPayload;
 import com.bada.badaback.global.annotation.ExtractToken;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "토큰 재발급", description = "TokenReissueApiController")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/token/reissue")
