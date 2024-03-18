7fa0918932c7b079048f8b4d6ba16f071a01d3bf
sunkyoung <yskkjdh@gmail.com>
2024-03-18 17:14:14 +0900

    refactor : 가족 조회 API 주소 수정

--- bada-back/src/main/java/com/bada/badaback/member/controller/MemberListApiController.java
+++ bada-back/src/main/java/com/bada/badaback/member/controller/MemberListApiController.java
@@ -2,7 +2,6 @@ package com.bada.badaback.member.controller;
 
 import com.bada.badaback.global.annotation.ExtractPayload;
 import com.bada.badaback.member.dto.MemberListResponseDto;
-import com.bada.badaback.member.dto.MemberResponseDto;
 import com.bada.badaback.member.service.MemberListService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -11,12 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
-@Tag(name = "MemberList", description = "MemberListApiController")
+@Tag(name = "FamilyList", description = "MemberListApiController")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/members")
+@RequestMapping("/api/family")
 public class MemberListApiController {
     private final MemberListService memberListService;
 
