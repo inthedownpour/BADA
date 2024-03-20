f8c03f3d9626e5232e6b67a55b1dab8374fa51c7
sunkyoung <yskkjdh@gmail.com>
2024-03-21 00:28:36 +0900

    refactor : 인증 코드 검증 시 10분이 지났다면 검증 실패하도록 메서드 수정
    Change-Id: I0ce43d827deea68ebf0cd55d11ee39f5e91cb01c

--- bada-back/src/main/java/com/bada/badaback/auth/exception/AuthErrorCode.java
+++ bada-back/src/main/java/com/bada/badaback/auth/exception/AuthErrorCode.java
@@ -11,7 +11,8 @@ public enum AuthErrorCode implements ErrorCode {
     AUTH_EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH_001", "토큰의 유효기간이 만료되었습니다."),
     AUTH_INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH_002", "토큰이 유효하지 않습니다."),
     INVALID_PERMISSION(HttpStatus.FORBIDDEN, "AUTH_003", "권한이 없습니다. 로그인 먼저 해주세요."),
-    AUTHCODE_NOT_FOUND(HttpStatus.NOT_FOUND, "AUTH_004", "인증 코드 정보를 찾을 수 없습니다.")
+    AUTHCODE_NOT_FOUND(HttpStatus.NOT_FOUND, "AUTH_004", "인증 코드 정보를 찾을 수 없습니다."),
+    AUTH_EXPIRED_AUTHCODE(HttpStatus.NOT_FOUND, "AUTH_005", "인증코드의 유효기간이 만료되었습니다.")
     ;
 
     private final HttpStatus status;
