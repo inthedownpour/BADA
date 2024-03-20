11101fd463abc8c7eba248326040ddf576891bcb
sunkyoung <yskkjdh@gmail.com>
2024-03-20 13:26:38 +0900

    feat : 마이플레이스 상세조회 및 전체조회 API 구현
    Change-Id: Ia501082893ad070a3ff034bf5059e1304132a44b

---
+++ bada-back/src/main/java/com/bada/badaback/myplace/dto/MyPlaceDetailResponseDto.java
@@ -0,0 +1,33 @@
+package com.bada.badaback.myplace.dto;
+
+import com.bada.badaback.myplace.domain.MyPlace;
+import lombok.Builder;
+
+@Builder
+public record MyPlaceDetailResponseDto(
+        Long myPlaceId,
+        String placeName,
+        String placeLatitude,
+        String placeLongitude,
+        String placeCategoryCode,
+        String placePhoneNumber,
+        String icon,
+        String familyCode,
+        String addressName,
+        String addressRoadName
+) {
+    public static MyPlaceDetailResponseDto from(MyPlace findMyPlace) {
+        return MyPlaceDetailResponseDto.builder()
+                .myPlaceId(findMyPlace.getId())
+                .placeName(findMyPlace.getPlaceName())
+                .placeLatitude(findMyPlace.getPlaceLatitude())
+                .placeLongitude(findMyPlace.getPlaceLongitude())
+                .placeCategoryCode(findMyPlace.getPlaceCategoryCode())
+                .placePhoneNumber(findMyPlace.getPlacePhoneNumber())
+                .icon(findMyPlace.getIcon())
+                .familyCode(findMyPlace.getFamilyCode())
+                .addressName(findMyPlace.getAddressName())
+                .addressRoadName(findMyPlace.getAddressRoadName())
+                .build();
+    }
+}
