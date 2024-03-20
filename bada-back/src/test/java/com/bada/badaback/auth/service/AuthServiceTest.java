e6eaf6639504452b49e62a697e638b935bbe316d
sunkyoung <yskkjdh@gmail.com>
2024-03-21 00:28:36 +0900

    refactor : 인증 코드 검증 시 10분이 지났다면 검증 실패하도록 메서드 수정
    Change-Id: I0ce43d827deea68ebf0cd55d11ee39f5e91cb01c

--- bada-back/src/test/java/com/bada/badaback/auth/service/AuthServiceTest.java
+++ bada-back/src/test/java/com/bada/badaback/auth/service/AuthServiceTest.java
@@ -3,19 +3,25 @@ package com.bada.badaback.auth.service;
 import com.bada.badaback.auth.domain.AuthCode;
 import com.bada.badaback.auth.domain.Token;
 import com.bada.badaback.auth.dto.LoginResponseDto;
+import com.bada.badaback.auth.exception.AuthErrorCode;
 import com.bada.badaback.common.ServiceTest;
+import com.bada.badaback.global.exception.BaseException;
 import com.bada.badaback.global.security.JwtProvider;
 import com.bada.badaback.member.domain.Member;
 import com.bada.badaback.member.domain.SocialType;
 import com.bada.badaback.member.service.MemberFindService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
 
 import static com.bada.badaback.feature.AuthCodeFixture.AUTHCODE_0;
 import static com.bada.badaback.feature.MemberFixture.SUNKYOUNG;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("Auth [Service Layer] -> AuthService 테스트")
@@ -39,7 +45,6 @@ public class AuthServiceTest extends ServiceTest {
     void setup() {
         member = memberRepository.save(SUNKYOUNG.toMember());
         authCode = authCodeRepository.save(AUTHCODE_0.toAuthCode(member));
-
     }
 
     @Test
@@ -63,48 +68,61 @@ public class AuthServiceTest extends ServiceTest {
         );
     }
 
-    @Test
-    @DisplayName("회원가입에 성공한다(기존 가족 그룹 가입)")
-    void join() {
-        // when
-        Long saveMemberId = authService.join("심지연", "010-2222-2222","def@naver.com", "KAKAO", null, authCode.getCode(), "fcmToken");
+    @Nested
+    @DisplayName("회원가입 (기존 가족 그룹 가입)")
+    class join {
+        @Test
+        @DisplayName("만료된 인증 코드로는 가입할 수 없다")
+        void throwExceptionByAuthCodeExpiredToken() {
+            // when - then
+            assertThatThrownBy(() -> authService.validateAuthCode(authCode.getCode(), LocalDateTime.now().plusMinutes(20)))
+                    .isInstanceOf(BaseException.class)
+                    .hasMessage(AuthErrorCode.AUTH_EXPIRED_AUTHCODE.getMessage());
+        }
 
-        // then
-        Member findMember = memberFindService.findById(saveMemberId);
-        String findFamilyCode = authCodeFindService.findMemberByCode(authCode.getCode()).getFamilyCode();
-        assertAll(
-                () -> assertThat(findMember.getName()).isEqualTo("심지연"),
-                () -> assertThat(findMember.getPhone()).isEqualTo("010-2222-2222"),
-                () -> assertThat(findMember.getEmail()).isEqualTo("def@naver.com"),
-                () -> assertThat(findMember.getSocial()).isEqualTo(SocialType.KAKAO),
-                () -> assertThat(findMember.getIsParent()).isEqualTo(1),
-                () -> assertThat(findMember.getProfileUrl()).isEqualTo(null),
-                () -> assertThat(findMember.getFamilyCode()).isEqualTo(findFamilyCode),
-                () -> assertThat(findMember.getMovingState()).isEqualTo(0),
-                () -> assertThat(findMember.getFcmToken()).isEqualTo("fcmToken")
-        );
-    }
+        @Test
+        @DisplayName("회원가입에 성공한다(기존 가족 그룹 가입)")
+        void success() {
+            // when
+            Long saveMemberId = authService.join("심지연", "010-2222-2222","def@naver.com", "KAKAO", null, authCode.getCode(), "fcmToken");
 
-    @Test
-    @DisplayName("아이 회원가입에 성공한다(기존 가족 그룹 가입)")
-    void joinChild() {
-        // when
-        Long saveMemberId = authService.joinChild("심지연", "010-2222-2222", null, authCode.getCode(), "fcmToken");
+            // then
+            Member findMember = memberFindService.findById(saveMemberId);
+            String findFamilyCode = authCodeFindService.findMemberByCode(authCode.getCode()).getFamilyCode();
+            assertAll(
+                    () -> assertThat(findMember.getName()).isEqualTo("심지연"),
+                    () -> assertThat(findMember.getPhone()).isEqualTo("010-2222-2222"),
+                    () -> assertThat(findMember.getEmail()).isEqualTo("def@naver.com"),
+                    () -> assertThat(findMember.getSocial()).isEqualTo(SocialType.KAKAO),
+                    () -> assertThat(findMember.getIsParent()).isEqualTo(1),
+                    () -> assertThat(findMember.getProfileUrl()).isEqualTo(null),
+                    () -> assertThat(findMember.getFamilyCode()).isEqualTo(findFamilyCode),
+                    () -> assertThat(findMember.getMovingState()).isEqualTo(0),
+                    () -> assertThat(findMember.getFcmToken()).isEqualTo("fcmToken")
+            );
+        }
 
-        // then
-        Member findMember = memberFindService.findById(saveMemberId);
-        String findFamilyCode = authCodeFindService.findMemberByCode(authCode.getCode()).getFamilyCode();
-        assertAll(
-                () -> assertThat(findMember.getName()).isEqualTo("심지연"),
-                () -> assertThat(findMember.getPhone()).isEqualTo("010-2222-2222"),
-                () -> assertThat(findMember.getEmail()).isNotNull(),
-                () -> assertThat(findMember.getSocial()).isEqualTo(SocialType.CHILD),
-                () -> assertThat(findMember.getIsParent()).isEqualTo(0),
-                () -> assertThat(findMember.getProfileUrl()).isEqualTo(null),
-                () -> assertThat(findMember.getFamilyCode()).isEqualTo(findFamilyCode),
-                () -> assertThat(findMember.getMovingState()).isEqualTo(0),
-                () -> assertThat(findMember.getFcmToken()).isEqualTo("fcmToken")
-        );
+        @Test
+        @DisplayName("아이 회원가입에 성공한다(기존 가족 그룹 가입)")
+        void joinChild() {
+            // when
+            Long saveMemberId = authService.joinChild("심지연", "010-2222-2222", null, authCode.getCode(), "fcmToken");
+
+            // then
+            Member findMember = memberFindService.findById(saveMemberId);
+            String findFamilyCode = authCodeFindService.findMemberByCode(authCode.getCode()).getFamilyCode();
+            assertAll(
+                    () -> assertThat(findMember.getName()).isEqualTo("심지연"),
+                    () -> assertThat(findMember.getPhone()).isEqualTo("010-2222-2222"),
+                    () -> assertThat(findMember.getEmail()).isNotNull(),
+                    () -> assertThat(findMember.getSocial()).isEqualTo(SocialType.CHILD),
+                    () -> assertThat(findMember.getIsParent()).isEqualTo(0),
+                    () -> assertThat(findMember.getProfileUrl()).isEqualTo(null),
+                    () -> assertThat(findMember.getFamilyCode()).isEqualTo(findFamilyCode),
+                    () -> assertThat(findMember.getMovingState()).isEqualTo(0),
+                    () -> assertThat(findMember.getFcmToken()).isEqualTo("fcmToken")
+            );
+        }
     }
 
     @Test
