6591aa3ad92af062d2173501f8d86813a187ad71
SIMJIYEON <sim.yyeon@gmail.com>
2024-03-25 15:13:55 +0900

    feat: 경로추천 api 중간 경유지 선정
    Change-Id: I28afbfb9de8b3f708dabe2cab564103b83effb5c

--- bada-back/src/main/java/com/bada/badaback/safefacility/domain/SafeFacilityRepository.java
+++ bada-back/src/main/java/com/bada/badaback/safefacility/domain/SafeFacilityRepository.java
@@ -7,12 +7,8 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface SafeFacilityRepository extends JpaRepository<SafeFacility, Long> {
-    //    @Query(value = "SELECT sf FROM SafeFacility sf WHERE sf.type = 'cctv'")
-
-//    @Query(value ="select * from safe_facility sf where sf.type ='cctv'",nativeQuery = true)
-
 //    @Query(value ="SELECT sf FROM SafeFacility sf WHERE sf.type = 'cctv'"+
-//            "AND ST_Distance_Sphere(Point(:middleLong,:middleLat),POINT(sf.facilityLongitude, sf.facilityLatitude)) <= 0.3")
+//            "AND ST_Distance_Sphere(Point(:middleLong,:middleLat),POINT(sf.facilityLongitude, sf.facilityLatitude)) <= 0.3 * 1000")
 
         @Query(value ="select * from safe_facility sf where sf.type ='cctv' and" +
             "( 6371 * acos (" +
@@ -22,4 +18,20 @@ public interface SafeFacilityRepository extends JpaRepository<SafeFacility, Long
             "+ sin ( radians(sf.facility_latitude) )" +
             "* sin( radians(:middleLat) ))) <= :distance",nativeQuery = true)
     List<SafeFacility> getCCTVs(@Param("middleLat") String middleLat, @Param("middleLong") String middleLong,@Param("distance") Double distance);
+    @Query(value ="select * from safe_facility sf where sf.type ='guard' and" +
+            "( 6371 * acos (" +
+            "cos ( radians(sf.facility_latitude) )" +
+            "* cos( radians(:middleLat) )" +
+            "* cos( radians(sf.facility_longitude) - radians(:middleLong) )" +
+            "+ sin ( radians(sf.facility_latitude) )" +
+            "* sin( radians(:middleLat) ))) <= :distance",nativeQuery = true)
+    List<SafeFacility> getGuard(@Param("middleLat") String middleLat, @Param("middleLong") String middleLong,@Param("distance") Double distance);
+    @Query(value ="select * from safe_facility sf where sf.type ='police' and" +
+            "( 6371 * acos (" +
+            "cos ( radians(sf.facility_latitude) )" +
+            "* cos( radians(:middleLat) )" +
+            "* cos( radians(sf.facility_longitude) - radians(:middleLong) )" +
+            "+ sin ( radians(sf.facility_latitude) )" +
+            "* sin( radians(:middleLat) ))) <= :distance",nativeQuery = true)
+    List<SafeFacility> getPolice(@Param("middleLat") String middleLat, @Param("middleLong") String middleLong,@Param("distance") Double distance);
 }
