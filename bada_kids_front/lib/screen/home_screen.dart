0671945603a7ded4cd6a9d155f99e32de4ffc543
이창헌 <changbill@naver.com>
2024-03-25 15:03:20 +0900

    feat : 가족 리스트 불러오기 api 수정, member model 수정
    Change-Id: Ib20c5113f4c1f5d859b4c5e8715fcdd02cf3f69e

--- bada_kids_front/lib/screen/home_screen.dart
+++ bada_kids_front/lib/screen/home_screen.dart
@@ -32,10 +32,10 @@ class _HomeScreenState extends State<HomeScreen> {
   void initState() {
     super.initState();
     _loadProfileFromBackEnd();
-    loadFamilyList;
+    loadFamilyList();
   }
 
-  void _loadProfileFromBackEnd() async {
+  Future<void> _loadProfileFromBackEnd() async {
     String? accessToken = await _storage.read(key: 'accessToken');
     debugPrint('accessToken: $accessToken');
 
@@ -56,12 +56,13 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
-  void loadFamilyList() async {
+  Future<void> loadFamilyList() async {
     String? accessToken = await _storage.read(key: 'accessToken');
+    debugPrint('accessToken: $accessToken');
 
     // HTTP GET 요청을 보냅니다.
     final response = await http.get(
-      Uri.parse('https://j10b207.p.ssafy.io/api/family/parentlist'),
+      Uri.parse('https://j10b207.p.ssafy.io/api/family'),
       // 요청 헤더에 Authorization을 포함시킵니다.
       headers: {
         'Authorization': 'Bearer $accessToken',
@@ -172,37 +173,66 @@ class _HomeScreenState extends State<HomeScreen> {
                       } else if (snapshot.hasError) {
                         return const Text("데이터 로드에 실패했습니다.");
                       } else if (snapshot.hasData) {
-                        return ListView.builder(
+                        return GridView.builder(
                           itemCount: snapshot.data!.length,
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            childAspectRatio:
+                                (deviceWidth / 2) / (deviceHeight * 0.25),
+                            crossAxisSpacing: 10,
+                            mainAxisSpacing: 10,
+                          ),
                           itemBuilder: (context, index) {
                             Member member = snapshot.data![index];
-                            return SizedBox(
-                              width: deviceWidth * 0.8,
-                              height: deviceHeight * 0.1,
-                              child: ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: Colors.blue, // 버튼의 배경색 설정
-                                  foregroundColor: Colors.white, // 버튼의 글자색 설정
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(
-                                        20), // 모서리의 둥근 정도 설정
-                                  ),
-                                  elevation: 5, // 버튼의 그림자 깊이 설정
+                            // 아이템 내용 구성
+                            return ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: const Color(0xff7B79FF),
+                                foregroundColor: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
                                 ),
-                                onPressed: () {
-                                  () async {
-                                    final Uri url = Uri(
-                                      scheme: 'tel',
-                                      path: member.phone,
-                                    );
-                                    if (await canLaunchUrl(url)) {
-                                      await launchUrl(url);
-                                    } else {
-                                      debugPrint('전화 걸기 실패');
-                                    }
-                                  };
-                                },
-                                child: Text(member.name),
+                                elevation: 5,
+                              ),
+                              onPressed: () async {
+                                final Uri url = Uri(
+                                  scheme: 'tel',
+                                  path: member.phone,
+                                );
+                                if (await canLaunchUrl(url)) {
+                                  await launchUrl(url);
+                                } else {
+                                  debugPrint('전화 걸기 실패');
+                                }
+                              },
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  CircleAvatar(
+                                    radius: 45,
+                                    backgroundImage: member.profileUrl != null
+                                        ? NetworkImage(member.profileUrl!)
+                                        : Image.asset(
+                                                'assets/img/default_profile.png')
+                                            .image,
+                                  ),
+                                  SizedBox(height: deviceHeight * 0.02),
+                                  // 이름과 아이콘을 수평으로 배치
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      const Icon(Icons.phone,
+                                          size: 20), // 전화 아이콘
+                                      const SizedBox(
+                                          width: 8), // 아이콘과 이름 사이의 간격
+                                      Text(
+                                        member.name,
+                                        style: const TextStyle(fontSize: 15),
+                                      ),
+                                    ],
+                                  ),
+                                ],
                               ),
                             );
                           },
