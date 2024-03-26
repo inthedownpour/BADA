015c134d839b515265e747bc0ef39cb40bb12a5f
SIMJIYEON <sim.yyeon@gmail.com>
2024-03-26 22:53:59 +0900

    feat: 경로 api 설계

---
+++ bada-back/src/main/java/com/bada/badaback/route/controller/RouteController.java
@@ -0,0 +1,55 @@
+package com.bada.badaback.route.controller;
+
+import com.bada.badaback.global.annotation.ExtractPayload;
+import com.bada.badaback.route.dto.RouteRequestDto;
+import com.bada.badaback.route.dto.RouteResponseDto;
+import com.bada.badaback.route.service.RouteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@Controller
+@RequestMapping("/api/route")
+@RequiredArgsConstructor
+public class RouteController {
+    private final RouteService routeService;
+
+    /**
+     * 아이가 자신의 현재 경로를 생성한다
+     * @param memberId
+     * @param routeRequestDto
+     * @return
+     * @throws IOException
+     */
+    @PostMapping
+    public ResponseEntity<Void> creteRoute(@ExtractPayload Long memberId, @RequestBody RouteRequestDto routeRequestDto) throws IOException {
+        routeService.createRoute(memberId, routeRequestDto);
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * 부모의 멤버id를 받아서 현재 사용자와 아이의 멤버id가 같은 가족이라면 경로를 조회한다.
+     * @param memberId
+     * @param childId
+     * @return
+     */
+    @GetMapping("/{childId}")
+    public ResponseEntity<RouteResponseDto> getRoute(@ExtractPayload Long memberId, @PathVariable("childId") Long childId) {
+        RouteResponseDto routeResponseDto = routeService.getRoute(memberId, childId);
+        return ResponseEntity.ok(routeResponseDto);
+    }
+
+    /**
+     * 아이가 도착을 누르면 아이의 경로를 삭제한다
+     * @param memberId
+     * @return
+     */
+    @DeleteMapping
+    public ResponseEntity<Void> deleteRoute(@ExtractPayload Long memberId) {
+        routeService.deleteRoute(memberId);
+        return ResponseEntity.ok().build();
+    }
+}
