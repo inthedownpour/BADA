8624e148506e135316202d72b270522ffae252bf
sunkyoung <yskkjdh@gmail.com>
2024-03-26 09:33:50 +0900

    feat : 아이 현재 위치 (CurrentLocation) 등록, 수정, 삭제, 조회 API 구현
    Change-Id: I3d3c691b5f166ecfaeed1e8d06e96df4cec28105

---
+++ bada-back/src/main/java/com/bada/badaback/currentLocation/controller/CurrentLocationApiController.java
@@ -0,0 +1,46 @@
+package com.bada.badaback.currentLocation.controller;
+
+import com.bada.badaback.currentLocation.dto.CurrentLocationRequestDto;
+import com.bada.badaback.currentLocation.dto.CurrentLocationResponseDto;
+import com.bada.badaback.currentLocation.service.CurrentLocationService;
+import com.bada.badaback.global.annotation.ExtractPayload;
+import com.bada.badaback.member.service.MemberService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "최근 위치", description = "CurrentLocationApiController")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/currentLocation")
+public class CurrentLocationApiController {
+    private final CurrentLocationService currentLocationService;
+    private final MemberService memberService;
+
+    @PostMapping
+    public ResponseEntity<Void> create(@ExtractPayload Long memberId, @RequestBody @Valid CurrentLocationRequestDto requestDto) {
+        currentLocationService.create(memberId, requestDto.currentLatitude(), requestDto.currentLongitude());
+        memberService.updateMovingState(memberId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping
+    public ResponseEntity<Void> update(@ExtractPayload Long memberId, @RequestBody @Valid CurrentLocationRequestDto requestDto) {
+        currentLocationService.update(memberId, requestDto.currentLatitude(), requestDto.currentLongitude());
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> delete(@ExtractPayload Long memberId) {
+        currentLocationService.delete(memberId);
+        memberService.updateMovingState(memberId);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{childId}")
+    public ResponseEntity<CurrentLocationResponseDto> readCurrentLocation(@ExtractPayload Long memberId, @PathVariable("childId") Long childId) {
+        return ResponseEntity.ok(currentLocationService.read(memberId, childId));
+    }
+}
