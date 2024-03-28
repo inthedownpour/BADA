11b993ac680b0703f3de9937e8a5ee0139fac5c8
sunkyoung <yskkjdh@gmail.com>
2024-03-28 17:11:52 +0900

    feat : 기존 회원 정보 수정 API에 사용되는 requestDto에 childId 추가해서 아이 정보 수정 API 구현
    Change-Id: I9c2b529f0ac05eca238af837d159439a6bdf6296

--- bada-back/src/main/java/com/bada/badaback/member/exception/MemberErrorCode.java
+++ bada-back/src/main/java/com/bada/badaback/member/exception/MemberErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum MemberErrorCode implements ErrorCode {
-    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_001", "회원 정보를 찾을 수 없습니다.")
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_001", "회원 정보를 찾을 수 없습니다."),
+    MEMBER_IS_NOT_CHILD_PARENT(HttpStatus.BAD_REQUEST, "MEMBER_002", "본인 패밀리 그룹에 속한 아이의 정보만 수정할 수 있습니다.")
     ;
 
     private final HttpStatus status;
