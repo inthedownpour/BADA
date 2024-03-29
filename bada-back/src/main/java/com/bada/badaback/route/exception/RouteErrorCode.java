00a281a8b0167996fb82f4ba3dbc48ace2e0904d
SIMJIYEON <sim.yyeon@gmail.com>
2024-03-29 13:19:21 +0900

    edit: responseDto에 lineString 추가

--- bada-back/src/main/java/com/bada/badaback/route/exception/RouteErrorCode.java
+++ bada-back/src/main/java/com/bada/badaback/route/exception/RouteErrorCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum RouteErrorCode implements ErrorCode {
     NOT_FAMILY(HttpStatus.NOT_FOUND,"ROUTE_001","가족이 아닙니다."),
-    ROUTE_NOT_FOUND(HttpStatus.NOT_FOUND,"ROUTE_002","회원의 상태를 찾을 수 없습니다.");
+    ROUTE_NOT_FOUND(HttpStatus.NOT_FOUND,"ROUTE_002","회원의 경로를 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String errorCode;
