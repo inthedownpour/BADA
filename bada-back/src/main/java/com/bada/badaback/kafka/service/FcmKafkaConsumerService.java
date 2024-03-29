5f501de766c5bc325eca1a9358f82bfca6a0892d
이용준 <sark2@naver.com>
2024-03-29 11:05:36 +0900

    feat: AlarmDto 객체 수정 경로 이탈 감지 알림 구현
    Change-Id: I42578d54b4270ec517f05bd78bcc20f282baf298

--- bada-back/src/main/java/com/bada/badaback/kafka/service/FcmKafkaConsumerService.java
+++ bada-back/src/main/java/com/bada/badaback/kafka/service/FcmKafkaConsumerService.java
@@ -20,26 +20,15 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @RequiredArgsConstructor
 public class FcmKafkaConsumerService {
-
   private final FcmService fcmService;
   private final AlarmLogService alarmLogService;
   private final MemberRepository memberRepository;
 
 
-  private static boolean isOffCourse(String latitude, String longitude){
-    return true;
-  }
-
   @KafkaListener(topics = "${spring.kafka.topics.now-topic}", groupId = "foo")
   public void alarmListenAndSend(AlarmDto alarmDto) throws IOException {
     log.info("################## 알림 수신 완료!!! alarmDto toString : {}", alarmDto);
 
-    if ( !isOffCourse(alarmDto.getLatitude(), alarmDto.getLongitude()) ) { // 정상범위 내 존재
-      log.info("################## 아이는 정상범위 내에 있습니다. ");
-      throw new BaseException(AlarmErrorCode.IS_OK);
-    }
-
-    log.info("!!!!!!!!!! 아이 경로 이탈 감지 !!!!!!!!!");
     List<Member> memberList = memberRepository.familyList(alarmDto.getFamilyCode());  // 알림을 보낼 패밀리의 맴버들을 가져오기
     if (memberList.isEmpty()) {
       log.info("##################  조회되는 패밀리 코드가 없습니다."); // 패밀리 코드 없으면 안내려가고 거르기.
@@ -53,14 +42,13 @@ public class FcmKafkaConsumerService {
       log.info("##################  member.toString() : {}", member.toString());
       fcmService.sendMessageTo(alarmDto, member);
 
-      // 알림 로그 기록
-      /** 알림 전송 확인까지 보류 */
-//      AlarmLogRequestDto alarmLogRequestDto = AlarmLogRequestDto.builder()
-//          .memberId(member.getId())
-//          .type(alarmDto.getType())
-//          .build();
-//      log.info("################## alarmLogRequestDto : {} ", alarmLogRequestDto.toString() );
-//      alarmLogService.writeAlarmLog(alarmLogRequestDto);
+      // 알림 로그 기록 /** 알림 전송 확인까지 보류 */
+      AlarmLogRequestDto alarmLogRequestDto = AlarmLogRequestDto.builder()
+          .memberId(member.getId())
+          .type(alarmDto.getType())
+          .build();
+      log.info("################## alarmLogRequestDto : {} ", alarmLogRequestDto.toString() );
+      alarmLogService.writeAlarmLog(alarmLogRequestDto);
 
     }
   }
