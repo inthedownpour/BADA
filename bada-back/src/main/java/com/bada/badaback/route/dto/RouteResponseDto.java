2bec39d757caada1c983199a394111d146251b9c
SIMJIYEON <sim.yyeon@gmail.com>
2024-04-01 00:08:40 +0900

    refactor: route entity에 출발지, 도착지 이름 저장

--- bada-back/src/main/java/com/bada/badaback/route/dto/RouteResponseDto.java
+++ bada-back/src/main/java/com/bada/badaback/route/dto/RouteResponseDto.java
@@ -12,14 +12,18 @@ public record RouteResponseDto(
         double startLat,
         double endLng,
         double endLat,
+        String addressName,
+        String placeName,
         List<Point> pointList
 ) {
-    public static RouteResponseDto from(double startLat, double startLng, double endLat, double endLng, List<Point> pointList) {
+    public static RouteResponseDto from(double startLat, double startLng, double endLat, double endLng, List<Point> pointList, String addressName, String placeName) {
         return RouteResponseDto.builder().startLng(startLng)
                 .startLat(startLat)
                 .endLng(endLng)
                 .endLat(endLat)
                 .pointList(pointList)
+                .addressName(addressName)
+                .placeName(placeName)
                 .build();
     }
 }
