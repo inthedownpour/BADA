f7a0dbabc95641d57f879abb879b700c39ae778e
SIMJIYEON <sim.yyeon@gmail.com>
2024-04-01 14:24:26 +0900

    refactor: 이미 경로가 존재할 경우 예외 처리

--- bada-back/src/main/java/com/bada/badaback/route/exception/RouteErrorCode.java
+++ bada-back/src/main/java/com/bada/badaback/route/exception/RouteErrorCode.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 public enum RouteErrorCode implements ErrorCode {
     NOT_FAMILY(HttpStatus.NOT_FOUND,"ROUTE_001","가족이 아닙니다."),
     ROUTE_NOT_FOUND(HttpStatus.NOT_FOUND,"ROUTE_002","회원의 경로를 찾을 수 없습니다."),
-    ALREADY_EXIST_ROUTE(HttpStatus.NOT_FOUND, "ROUTE_003","이미 회원의 경로가 존재합니다. 삭제 후 다시 시도해주세요.");
+    ALREADY_EXIST_ROUTE(HttpStatus.BAD_REQUEST, "ROUTE_003","이미 회원의 경로가 존재합니다. 삭제 후 다시 시도해주세요.");
 
     private final HttpStatus status;
     private final String errorCode;
