d8ae60bb8987072087f8db88c34c316c7219fd93
이용준 <sark2@naver.com>
2024-04-01 17:14:15 +0900

    refactor: Transactional 추가 및 AlarmLogService SQL 오류 수정
    Change-Id: Id627bc7634fc6bc5e92f398078c17f1866259a35

--- bada-back/src/main/java/com/bada/badaback/alarmlog/domain/AlarmLogRepository.java
+++ bada-back/src/main/java/com/bada/badaback/alarmlog/domain/AlarmLogRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AlarmRepository extends JpaRepository<AlarmLog, Long> {
+public interface AlarmLogRepository extends JpaRepository<AlarmLog, Long> {
 
 //  @Query(nativeQuery = true, value = "select * from alarm_log a where a.member_id = :memberId")
 //  List<AlarmLog> findAllAlarmLogsByMemberId(@Param("memberId") Long memberId, @Param("childId") Long childId);
