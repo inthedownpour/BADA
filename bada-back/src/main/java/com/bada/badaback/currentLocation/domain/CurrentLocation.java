91e76dd63b9bfa3c68fcfac10d3562c71cdf4042
sunkyoung <yskkjdh@gmail.com>
2024-04-03 16:22:14 +0900

    refactor : 현재위치 생성,수정 시간 받도록 수정

--- bada-back/src/main/java/com/bada/badaback/currentLocation/domain/CurrentLocation.java
+++ bada-back/src/main/java/com/bada/badaback/currentLocation/domain/CurrentLocation.java
@@ -1,5 +1,6 @@
 package com.bada.badaback.currentLocation.domain;
 
+import com.bada.badaback.global.BaseTimeEntity;
 import com.bada.badaback.member.domain.Member;
 import jakarta.persistence.*;
 import lombok.Getter;
@@ -9,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor
 @Table(name="current_location")
-public class CurrentLocation {
+public class CurrentLocation extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "current_location_id")
